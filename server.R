4ea9375eb0f7920cff24dbc63a2ca9ab29e090a4
atrubin <alexander.trubin@gmail.com>
2015-08-23 23:43:49 +0500

    Plotting system changed (from base to ggplot2). Checkgroup box added.

--- server.R
+++ server.R
@@ -1,27 +1,64 @@
 library(shiny)
+library(ggplot2)
+library(Rssa)
 
 msurl1 <- 'http://minsvyaz.ru/opendata/7710474375-dohodiuslugsvyazi/data-' 
 msurl2 <- '-structure-1.csv'
 
 shinyServer(function(input, output) {
     output$MetricPlot <- renderPlot( {
-        Field <- input$var
+        PredictionPeriod <- 4 * input$num
         
         NumData <- c()
         
-        for(i in 1:9)
-        {
-            msurl <- paste(msurl1, i, msurl2, sep="")
-            TT <- read.csv(file = msurl, header=F, fileEncoding='cp1251', dec=',', sep=';', skip = 1)
-            v2 <- gsub(pattern = " ", replacement = "", as.character(TT$V2))
-            v2 <- as.numeric(gsub(pattern = ",", replacement = ".", v2))
+        url1 <- 'http://minsvyaz.ru/opendata/7710474375-dohodiuslugsvyazi/data-9-structure-1.csv' 
+        df <- read.csv(file = url1, header=F, fileEncoding='cp1251', dec=',', sep=';', skip = 1)
+        Field <- as.character(df$V1)
+        Field <- Field[unlist(input$var)]
+        
+        pcolour <- vector()
+        p <- vector()
+        metric <- vector()
+        x <- vector()
+
+        for(f in Field) {
+            NumData <- c()
+            for(i in 1:9) {
+                msurl <- paste(msurl1, i, msurl2, sep="")
+                TT <- read.csv(file = msurl, header=F, fileEncoding='cp1251', dec=',', sep=';', skip = 1)
+                v2 <- gsub(pattern = " ", replacement = "", as.character(TT$V2))
+                v2 <- as.numeric(gsub(pattern = ",", replacement = ".", v2))
+                
+                n <- which(TT$V1 == f)
+                
+                NumData <- c(NumData, v2[n])
+            }
             
-            n <- which(TT$V1 == Field)
             
-            NumData <- c(NumData, v2[n])
+            r <- NumData
+            
+            if(length(NumData) == 9) {
+                s <- ssa(NumData, L=5)
+                r <- rforecast(s, groups = list(Trend = c(1:4)), len = PredictionPeriod, only.new = F)
+            } 
+            
+            
+            r[r < 0] <- 0
+            
+            
+            p <- c(p, r)
+            metric <- c(metric, rep(f, length(r)))
+            pcolour <- c(pcolour, rep("Source Data",length(NumData)))
+            if(length(NumData) == 9) pcolour <- c(pcolour, rep("Prediction", PredictionPeriod))
+            x <- c(x, 1:length(r))
         }
         
-        barplot(NumData, main = Field, ylab = 'Revenue', xlab = 'Q2 2013  -  Q2 2015')
+        g = ggplot(data.frame(y = p, x = x, c = pcolour, m = metric ), aes(x = x, y = y, Group = m))
+        g = g + ylab("Revenue in Ru")
+        g = g + xlab("Date (starting from Q2 2013)")
+        g = g + geom_line(aes (colour = m)) 
+        g = g + geom_point(size=3, shape=21, fill=c , aes(colour = c) ) 
+        g
         
     }
     )
--- ui.R
+++ ui.R
@@ -5,16 +5,21 @@ msurl2 <- '-structure-1.csv'
 msurl <- paste(msurl1, 9, msurl2, sep="")
 
 TT <- read.csv(file = msurl, header=F, fileEncoding='cp1251', dec=',', sep=';', skip = 1)
-mschoices <- c(as.character(TT$V1))
-
-
+mschoices <- as.character(TT$V1)
 
 shinyUI(pageWithSidebar(
 	headerPanel("Ministry of Telecom and Mass Communications of the Russian Federation. Information about revenue from telecommunication services."),
 	sidebarPanel(h3('Metric name'),
-	             selectInput("var", 
-	                         label = "Please, choose metric",
-	                         choices = mschoices)),
+# 	             selectInput("var", 
+# 	                         label = "Please, choose metric",
+# 	                         choices = mschoices),
+	             checkboxGroupInput("var", label = h3("Please, choose the metrics"), 
+	                                choices = as.list(setNames(1:length(mschoices),mschoices)),
+	                                selected = 1),
+	             hr(),
+	             sliderInput("num", label = "Prediction period (years)", min = 1, 
+	                         max = 15, value = 2)
+	             ),
 	mainPanel(h3('Data for the reporting period'),
 			  plotOutput("MetricPlot"))
 ))
