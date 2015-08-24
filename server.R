aeef9804a1276ec9d92605e7d9e95eda516eb264
atrubin <alexander.trubin@gmail.com>
2015-08-24 14:09:14 +0500

    Minor fixes.

--- server.R
+++ server.R
@@ -14,37 +14,38 @@ shinyServer(function(input, output) {
         url1 <- 'http://minsvyaz.ru/opendata/7710474375-dohodiuslugsvyazi/data-9-structure-1.csv' 
         df <- read.csv(file = url1, header=F, fileEncoding='cp1251', dec=',', sep=';', skip = 1)
         Field <- as.character(df$V1)
-        Field <- Field[unlist(input$var)]
+        Field <- Field[as.numeric(input$var)]
         
         pcolour <- vector()
         p <- vector()
         metric <- vector()
         x <- vector()
+        v2 <- vector()
+        v1 <- vector()
 
+        for(i in 1:9) {
+            msurl <- paste(msurl1, i, msurl2, sep="")
+            TT <- read.csv(file = msurl, header=F, fileEncoding='cp1251', dec=',', sep=';', skip = 1)
+            v <- gsub(pattern = " ", replacement = "", as.character(TT$V2))
+            v <- as.numeric(gsub(pattern = ",", replacement = ".", v))
+            v2 <- c(v2, v)
+            v1 <- c(v1, as.character(TT$V1))
+        }
+        
         for(f in Field) {
             NumData <- c()
-            for(i in 1:9) {
-                msurl <- paste(msurl1, i, msurl2, sep="")
-                TT <- read.csv(file = msurl, header=F, fileEncoding='cp1251', dec=',', sep=';', skip = 1)
-                v2 <- gsub(pattern = " ", replacement = "", as.character(TT$V2))
-                v2 <- as.numeric(gsub(pattern = ",", replacement = ".", v2))
-                
-                n <- which(TT$V1 == f)
-                
-                NumData <- c(NumData, v2[n])
-            }
             
-            
+            n <- which(v1 == f)
+            NumData <- c(NumData, v2[n])
+
             r <- NumData
             
             if(length(NumData) == 9) {
                 s <- ssa(NumData, L=5)
                 r <- rforecast(s, groups = list(Trend = c(1:4)), len = PredictionPeriod, only.new = F)
             } 
-            
-            
+
             r[r < 0] <- 0
-            
             
             p <- c(p, r)
             metric <- c(metric, rep(f, length(r)))
@@ -54,10 +55,10 @@ shinyServer(function(input, output) {
         }
         
         g = ggplot(data.frame(y = p, x = x, c = pcolour, m = metric ), aes(x = x, y = y, Group = m))
-        g = g + ylab("Revenue in Ru")
-        g = g + xlab("Date (starting from Q2 2013)")
+        g = g + ylab("Revenue in RUB")
+        g = g + xlab(paste("Date (starting from Q2 2013 to Q2 ", (2015 + input$num), ")" , sep=""))
         g = g + geom_line(aes (colour = m)) 
-        g = g + geom_point(size=3, shape=21, fill=c , aes(colour = c) ) 
+        g = g + geom_point(size=3, shape=21, aes(colour = c) ) 
         g
         
     }
--- ui.R
+++ ui.R
@@ -8,18 +8,15 @@ TT <- read.csv(file = msurl, header=F, fileEncoding='cp1251', dec=',', sep=';', 
 mschoices <- as.character(TT$V1)
 
 shinyUI(pageWithSidebar(
-	headerPanel("Ministry of Telecom and Mass Communications of the Russian Federation. Information about revenue from telecommunication services."),
+	headerPanel("Ministry of Telecom and Mass Communications of the Russian Federation. Information about revenue from telecommunication services"),
 	sidebarPanel(h3('Metric name'),
-# 	             selectInput("var", 
-# 	                         label = "Please, choose metric",
-# 	                         choices = mschoices),
-	             checkboxGroupInput("var", label = h3("Please, choose the metrics"), 
+	             checkboxGroupInput("var", label = "Please, choose the metrics:", 
 	                                choices = as.list(setNames(1:length(mschoices),mschoices)),
 	                                selected = 1),
 	             hr(),
-	             sliderInput("num", label = "Prediction period (years)", min = 1, 
+	             sliderInput("num", label = "Prediction period (years):", min = 1, 
 	                         max = 15, value = 2)
 	             ),
-	mainPanel(h3('Data for the reporting period'),
+	mainPanel(h3('Data for the reporting and forecasting periods'),
 			  plotOutput("MetricPlot"))
 ))
